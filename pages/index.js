a591cf71bf13a8a4b410db1b6755166bf84b5bcb
debasishpadhan <papun1263@gmail.com>
2022-05-06 12:45:42 +0530

    git integration

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+	  Welcome to Git-Integration-Module!
         </h1>
 
         <p className={styles.description}>
